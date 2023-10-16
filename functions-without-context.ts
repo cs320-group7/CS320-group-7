6f2de40bc3ee5bf2e119772f84241c35ee55b357
Caio Carvalho <caiobiondi@uol.com.br>
2023-10-16 18:19:09 -0400

    Feat: Wrote tests for all update db query function through Prisma unit testing

--- functions-without-context.ts
+++ functions-without-context.ts
@@ -1,37 +1,74 @@
-import prisma from './client'
+import prisma from "./client";
 
 interface CreateUser {
-  userName: string
-  diets: string[]
+  userName: string;
 }
 
-export async function createUser(user: CreateUser) {
+export const createUser = async (user: CreateUser) => {
   return await prisma.user.create({
     data: {
       userName: user.userName,
-      UserDiet: {
-        create: { diets: user.diets }
-      }
     },
-  })
+  });
+};
+
+interface UpdateUserIntolerances {
+  id: number;
+  intolerances: string[];
 }
 
-interface updateUserDiets {
-  userName: string,
-  diets: string[],
+export const updateUserIntolerances = async (user: UpdateUserIntolerances) => {
+  const result = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      UserIntolerance: {
+        upsert: {
+          create: { intolerances: user.intolerances },
+          update: { intolerances: user.intolerances },
+        },
+      },
+    },
+  });
+  return result;
+};
+
+interface UpdateUserDiets {
+  id: number;
+  diets: string[];
 }
 
-export async function updateUserDiets(user: updateUserDiets) {
-  return await prisma.user.update({
-    where: {
-      userName: user.userName
-    },
+export const updateUserDiets = async (user: UpdateUserDiets) => {
+  const result = await prisma.user.update({
+    where: { id: user.id },
     data: {
       UserDiet: {
-        update: {
-          diets: user.diets
-        }
-      }
-    }
-  })
+        upsert: {
+          create: { diets: user.diets },
+          update: { diets: user.diets },
+        },
+      },
+    },
+  });
+  return result;
+};
+
+interface UpdateUserCookbook {
+  id: number;
+  recipeId: number;
+  title: string;
 }
+
+export const updateUserCookbook = async (user: UpdateUserCookbook) => {
+  const result = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      cookBook: {
+        connectOrCreate: {
+          where: { id: user.recipeId },
+          create: { id: user.recipeId, title: user.title },
+        },
+      },
+    },
+  });
+  return result;
+};
