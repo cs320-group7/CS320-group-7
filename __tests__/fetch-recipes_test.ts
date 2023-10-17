731855c3e64021cb44d68ed3fc19dad1ebd1fdd3
Caio Carvalho <caiobiondi@uol.com.br>
2023-10-17 08:52:55 -0400

    Test: Wrote tests for fetch functions

---
+++ __tests__/fetch-recipes_test.ts
@@ -0,0 +1,52 @@
+import { assert } from "console";
+
+import { getRecipeInformation, searchRecipes } from "../src/api/fetch-recipes";
+
+test("searchRecipes follows type specification ", () => {
+  const promise = searchRecipes("pasta", "tomato,cheese");
+
+  assert(typeof promise === "object" && typeof promise.then === "function");
+
+  return promise.then((result) => {
+    assert(Array.isArray(result));
+    assert(result.every((e) => typeof e === "number"));
+  });
+});
+
+test("getRecipeInformation follows type specification ", () => {
+  const promise = getRecipeInformation(716429);
+
+  assert(typeof promise === "object" && typeof promise.then === "function");
+
+  return promise.then((result) => {
+    assert(typeof result === "object");
+    let keys = Object.keys(result);
+    assert(keys.includes("id"));
+    assert(keys.includes("title"));
+    assert(keys.includes("sourceUrl"));
+    assert(keys.includes("image"));
+    assert(keys.includes("imageType"));
+  });
+});
+
+test("searchRecipes fails if !response.ok ", () => {
+  const promise = searchRecipes("", "");
+
+  return promise.then(
+    (result) => {
+      assert(false);
+    },
+    (reason) => assert(reason instanceof Error),
+  );
+});
+
+test("getRecipeInformation fails if !response.ok ", () => {
+  const promise = getRecipeInformation(0);
+
+  return promise.then(
+    (result) => {
+      assert(false);
+    },
+    (reason) => assert(reason instanceof Error),
+  );
+});
