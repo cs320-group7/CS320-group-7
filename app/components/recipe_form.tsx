35a9c15c54df917ffc9f7d2638f81df6cb84db0f
pkustura <pkustura@gmail.com>
2023-10-30 20:32:28 -0400

    Added functional diet and intolerance checkboxes to the form

--- app/components/recipe_form.tsx
+++ app/components/recipe_form.tsx
@@ -6,22 +6,53 @@ import { getUserDiets, getUserIntolerances } from "@/src/db/queries";
 import {Card, CardHeader, CardBody, Image, Button, ButtonGroup} from "@nextui-org/react";
 import {getRecipeInformation, searchRecipes, RecipeData} from "@/src/api/fetch-recipes";
 import {RecipeCard, RecipeList} from "./recipe_list"
+import {CheckboxGroup, Checkbox} from "@nextui-org/react";
 
 export function RecipeForm (props:{user:User, diets:{diets: string[]}, intolerances:{intolerances: string[]}}) {
-    const [diets, setDiets] = useState<{diets: string[]} | null>(props.diets);
-    const [intolerances, setIntolerances] = useState<{intolerances: string[]} | null>(props.intolerances);
+    const [diets, setDiets] = useState<{diets: string[]}>(props.diets);
+    const [intolerances, setIntolerances] = useState<{intolerances: string[]}>(props.intolerances);
     const [recipeList, setRecipeList] = useState<JSX.Element[] | null>(null);
+    const [selected, setSelected] = React.useState(["useDiets", "useIntolerances"]);
 
     async function pressed() {
-        let query = await searchRecipes("pasta", "tomato,onion", diets, intolerances) as RecipeData
+        let queryDiets = diets
+        let queryIntol = intolerances
+        if (!selected.includes("useDiets")){
+            queryDiets = {diets: []}
+        }
+        if (!selected.includes("useIntolerances")){
+            queryIntol = {intolerances: []}
+        }
+        let query = await searchRecipes("pasta", "tomato,onion", queryDiets, queryIntol) as RecipeData
         setRecipeList(await RecipeList({searchedRecipes:query}))
     }
 
+
     return (
         <>
+            
+            <div>diets: {diets.diets}</div>
+            <div>intolerances: {intolerances.intolerances}</div>
+            <Card>
+                <CardBody>
+                <CheckboxGroup 
+                label="Select query options" 
+                orientation="horizontal" 
+                color="secondary"
+                value={selected}
+                onValueChange={setSelected}
+                >
+
+                
+                <Checkbox defaultSelected value="useDiets" className="text-white">Use Diets</Checkbox>
+                <Checkbox defaultSelected value="useIntolerances" className="text-white">Use Intolerances</Checkbox>
+            </CheckboxGroup>
+            
             <Button onPress = {pressed} radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" >
                 do Query
             </Button>
+                </CardBody>
+            </Card> 
             <ul>{recipeList}</ul>
         </>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import { Session, getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { LoginButton, LogoutButton } from "./auth";
-import { RecipeList } from "./components/recipe_list"
 import { RecipeForm } from "./components/recipe_form"
-import { searchRecipes } from "../src/api/fetch-recipes"
 import { getUserDiets, getUserIntolerances } from "@/src/db/queries"; 
 import { User } from "@prisma/client";
 
@@ -27,16 +25,13 @@ export default async function Page() {
   const userDiets = await getUserDiets(user.id) as {diets: string[]};
   const userIntolerances = await getUserIntolerances(user.id) as {intolerances: string[]};
 
-  //const testQuery  = await searchRecipes("pasta", "tomato,onion", diets, intolerances);
-
   return (
     <main>
       <LoginButton />
       <LogoutButton />
       <div>Hello World</div> <pre>{JSON.stringify(session)}</pre>
-      <div>diets: </div>
       {/* <div>{JSON.stringify(testQuery)}</div> */}
-      <div>Query: pasta. Ingredients: tomato,cheese</div>
+      <div>Query: pasta. Ingredients: tomato,onion</div>
       {/* <div> # Results = {testQuery.totalResults}</div> */}
       <RecipeForm user={user} diets={userDiets} intolerances={userIntolerances} />
       {/* <RecipeCard title="title" img_src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/SMPTE_Color_Bars.svg/1200px-SMPTE_Color_Bars.svg.png" id={5} /> */}
