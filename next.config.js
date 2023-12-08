5c41bc31397862f553d5555d3911057258085712
pkustura <pkustura@gmail.com>
2023-12-07 23:15:56 -0500

    Workaround that should work better

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "bcrypt"],
   },
+  webpack: (config) => {
+    config.externals = [...config.externals, 'bcrypt'];
+    return config;
+  }
 };
 
 module.exports = nextConfig;
